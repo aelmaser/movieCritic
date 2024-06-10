3b5cce5ae1928b8113514918d3c7537c86656fd4
ELMASER <ELMASER@ELMASER>
2024-06-10 14:47:10 +0300

    Proje dosyası ekle.

---
+++ courseDotNET/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using courseDotNET.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace courseDotNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
